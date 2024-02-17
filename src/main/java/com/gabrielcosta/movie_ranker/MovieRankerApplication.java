6dce00089c0ad5cdd4d31edac87c1e663b2c5b07
gabrielgcosta <gabriel.costa@codenapp.com>
2024-02-16 21:09:19 -0300

    start project

---
+++ src/main/java/com/gabrielcosta/movie_ranker/MovieRankerApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielcosta.movie_ranker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRankerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRankerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielcosta/movie_ranker/MovieRankerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielcosta.movie_ranker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRankerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
